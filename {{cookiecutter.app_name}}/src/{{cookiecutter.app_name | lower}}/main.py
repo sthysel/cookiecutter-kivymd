e9d266ed97a55e2fdf8960e2ef7133677723d2a1
sthysel <sthysel@gmail>
2020-10-14 13:10:10 +0800

    adds src

--- {{cookiecutter.app_name}}/src/{{cookiecutter.app_name | lower}}/main.py
+++ {{cookiecutter.app_name}}/src/{{cookiecutter.app_name | lower}}/main.py
@@ -4,8 +4,5 @@ class {{cookiecutter.app_name}}App(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
 
-
-
 if __name__ == "__main__":
     {{cookiecutter.app_name}}App().run()
-
